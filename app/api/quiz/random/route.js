bb3c0e197bb1a3bce30a80bd9017cacf5a369180
kimatrocious <kimatrocious@gmail.com>
2024-01-29 12:39:08 +0000

    Final changes

---
+++ app/api/quiz/random/route.js
@@ -0,0 +1,21 @@
+/**
+ * Retrieves a random question from the quiz.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the ID of a randomly selected question, or an error response.
+ */
+
+import questions from '@/data/quiz.json'
+import { NextResponse } from 'next/server'
+
+// 📁 app/api/quiz/random/route.js
+
+export async function GET() {
+  try {
+    const random = Math.floor(Math.random() * questions.data.length)
+    return NextResponse.json({
+      randomQuestion: questions.data[random].id,
+    })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
